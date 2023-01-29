8cca930642096dd00748a018a3a9b512a0305298
Pontus Frendin <pontus@frend.in>
2023-01-29 20:18:26 +0100

    Working

--- WordCombiner.cs
+++ WordCombiner.cs
@@ -1,70 +1,110 @@
+using System.Text.Json;
+
 public class WordApprover
 {
     private readonly string[] wordList1;
     private readonly string[] wordList2;
+    private readonly FileInfo destinationFile;
+    private readonly FileInfo checkpointFile;
+    private readonly FileInfo tmpFile;
+    private HashSet<string> approved = new HashSet<string>();
+    private int checkpointSize = 50000000;
+    private CheckpointData checkpointData;
+    private readonly IEnumerable<PointCalculator> pointCalculators;
 
-    public int Size { get; }
-
-    private readonly DirectoryInfo dataDirectory;
-    private readonly string fileName;
-    private readonly int[] wordNumbers;
-
-    public WordApprover(string[] wordList1, string[] wordList2, int noSplits, string dataDirectory, string fileName, int[] wordNumbers)
+    public WordApprover(string[] wordList1, string[] wordList2, FileInfo destinationFile, int[] wordIndexes)
     {
+        this.pointCalculators = wordIndexes.Select(index => new PointCalculator(index));
+        this.destinationFile = destinationFile;
+        this.tmpFile = new FileInfo(Path.Combine(destinationFile.DirectoryName, destinationFile.Name + ".tmp"));
+
+        if (tmpFile.Exists)
+        {
+            approved = File.ReadLines(tmpFile.FullName).ToHashSet();
+        }
+        
         this.wordList1 = wordList1;
         this.wordList2 = wordList2;
-        Size = this.wordList2.Length / noSplits + 1;
-        this.dataDirectory = new DirectoryInfo(dataDirectory);
-        this.fileName = fileName;
-        this.wordNumbers = wordNumbers;
+        
+        this.checkpointFile = new FileInfo(Path.Combine(destinationFile.DirectoryName,
+                                                        $"{Path.GetFileNameWithoutExtension(destinationFile.Name)}.checkpoint"));
+        if (this.checkpointFile.Exists)
+        {
+            this.checkpointData = JsonSerializer.Deserialize<CheckpointData>(File.ReadAllText(this.checkpointFile.FullName));
+        }
+        else
+        {
+            this.checkpointData = new CheckpointData();
+        }
+        
     }
 
     public void WriteApproved() 
     {
-        Parallel.ForEach(wordList2.Chunk(Size).Select((words, index) => new {Index = index, Words = words}), chunk => 
+        var totalPermutations = (long)wordList1.Length * wordList2.Length;
+        Console.WriteLine($"{DateTime.Now} - {destinationFile.Name} - Börjar leta giltiga ord bland {totalPermutations:N0} möjliga kombinationer. Tidigare körningar hade redan hittat {approved.Count:N0} ord.");
+        
+        long iterations = 0;
+        
+        var totalStartTime = DateTime.Now;
+        var checkpointStartTime = totalStartTime;
+
+        var oldTmpFile = new FileInfo(tmpFile.FullName + ".old");
+        var newTmpFile = new FileInfo(tmpFile.FullName + ".new");
+
+
+        foreach (var p0 in wordList1)
         {
-            var totalPermutations = chunk.Words.Length * (long)wordList1.Length;
-            Console.WriteLine($"{DateTime.Now} - Börjar leta giltiga ord bland {totalPermutations:N} möjliga kombinationer");
-            var approved = new HashSet<string>();
-            long iterations = 0;
-            var resultFile = new FileInfo(Path.Combine(dataDirectory.FullName, $"{Path.GetFileNameWithoutExtension(fileName)}_Part{chunk.Index:D2}{Path.GetExtension(fileName)}"));
-            Console.WriteLine(resultFile);
-            var checkpointSize = 25000000;
-            
-            var totalStartTime = DateTime.Now;
-            var checkpointStartTime = totalStartTime;
-
-            foreach (var p0 in wordList1)
+            foreach (var p1 in wordList2)
             {
-                foreach (var p1 in chunk.Words)
+                iterations += 1;
+
+                if (this.checkpointData.Count > 0)
                 {
-                    iterations += 1;
-                    if (iterations % checkpointSize == 0)
-                    {
-                        var checkpointStopTime = DateTime.Now;
-                        var duration = checkpointStopTime - totalStartTime;
-                        Console.WriteLine($"{checkpointStopTime} - {resultFile.Name} - Gör {checkpointSize / (checkpointStopTime - checkpointStartTime).TotalSeconds:N0} test/sekund, har testat {iterations:N} kombinationer ({((double)iterations / (double)totalPermutations):P2}) och hittat {approved.Count:N} godkända efter {duration.Hours:D2}:{duration.Minutes:D2}:{duration.Seconds:D2}.");
+                    this.checkpointData.Count -= 1;
+                    continue;
+                }
 
-                        checkpointStartTime = DateTime.Now;
+                if (iterations % checkpointSize == 0)
+                {
+                    var checkpointStopTime = DateTime.Now;
+                    var duration = checkpointStopTime - totalStartTime;
+                    Console.WriteLine($"{checkpointStopTime} - {destinationFile.Name} - Gör {checkpointSize / (checkpointStopTime - checkpointStartTime).TotalSeconds:N0} test/sekund, har testat {iterations:N0} kombinationer ({((double)iterations / (double)totalPermutations):P2}) och hittat {approved.Count:N0} godkända efter {duration.Hours:D2}:{duration.Minutes:D2}:{duration.Seconds:D2}.");
+
+                    tmpFile.Refresh();
+                    if (tmpFile.Exists)
+                    {
+                        File.Move(tmpFile.FullName, oldTmpFile.FullName);
+                    }
+                    File.WriteAllLines(newTmpFile.FullName, approved);
+                    File.Move(newTmpFile.FullName, tmpFile.FullName);
+
+                    oldTmpFile.Refresh();
+                    if (oldTmpFile.Exists)
+                    {
+                        File.Delete(oldTmpFile.FullName);
                     }
 
-                    var combination = CombineWords(p0, p1);
+                    File.WriteAllText(this.checkpointFile.FullName, JsonSerializer.Serialize(new CheckpointData(iterations)));
 
-                    if (combination == null)
-                    {
-                        continue;
-                    }
+                    checkpointStartTime = DateTime.Now;
+                }
 
-                    // Lägg till poängkontroll
-                    foreach (var number in wordNumbers)
-                    {
+                var combination = CombineWords(p0, p1);
 
-                    }
+                if (combination == null)
+                {
+                    continue;
+                }
 
+                if (pointCalculators.All(calc => calc.IsApproved(combination)))
+                {
                     approved.Add(combination);
                 }
             }
-        });
+        }
+        Console.WriteLine($"{DateTime.Now} - {destinationFile.Name} - KLAR! Hittade {approved.Count()} gorkända ord.");
+        File.Move(tmpFile.FullName, this.destinationFile.FullName);
     }
 
     private string? CombineWords(string word1, string word2)
